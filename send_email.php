3bd9c4d8823fd4c55795a8d7796a7753c96a9c45
Pratiksha Navale <navalepratiksha16@gmail.com>
2025-01-09 17:58:00 +0530

    contact_form

---
+++ send_email.php
@@ -0,0 +1,25 @@
+<?
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    $to = "acharyarushi07@gmail.com";
+    $subject = "Contact Form Submistion";
+    $headers = "From:$email\r\n";
+    $headers.="Replay-to: $email\r\n";
+
+    $body = "Name: $name\nEmail: $email\n\n$message";
+
+    if (mail($to,$subject,$body,$headers)){
+        echo "Message sent successfully";
+
+    }else{
+        echo "Failed to send message";
+    }
+
+    
+}
+?>
